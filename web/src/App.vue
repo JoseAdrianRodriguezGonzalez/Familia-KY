1f7e4030368372961567f3c69d7c47b05bb9e912
Adrian <kerpador@gmail.com>
2024-07-28 16:10:33 -0600

    hello

--- web/src/App.vue
+++ web/src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style lang="scss">
 .logo {
   height: 6em;
   padding: 1.5em;
