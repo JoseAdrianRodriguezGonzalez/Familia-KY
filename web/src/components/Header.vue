90f9a8309723f72ae3dba5874c76b3c73f2804e1
Adrian <kerpador@gmail.com>
2024-07-29 00:28:07 -0600

    adding the new files

---
+++ web/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import  './Header.scss'
+
+</script>
+
+<template>
+  <nav>
+    <div class="Title">
+      KY-SENSORS
+    </div>
+    <div class="SECTIONS">
+      <div class="HOME">HOME</div>
+      <div class="INDEX">INDEX</div>
+    </div>
+  </nav>
+</template>
+
+
