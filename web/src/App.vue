bfe3f0f69ee8f7086bbc21a0e61618617a939dad
Adrian <kerpador@gmail.com>
2024-07-31 16:05:05 -0600

    adding the first step of the swebsite

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 </script>
 
 <template>
   <Header></Header>
-
+  <Main></Main>
 </template>
 
 <style lang="scss">
---
+++ web/src/components/main.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import  './Main.scss'
+
+</script>
+<template>
+    <main>
+        <div class="nav">
+            <ol>
+                <li>KY-01</li>
+                <li>KY-02</li>
+                <li>KY-03</li>
+                <li>KY-04</li>
+                <li>KY-05</li>
+                <li>KY-06</li>
+                <li>KY-07</li>
+                <li>KY-08</li>
+                <li>KY-09</li>
+                <li>KY-10</li>
+                <li>KY-11</li>
+                <li>KY-12</li>
+                <li>KY-13</li>
+                <li>KY-14</li>
+                <li>KY-15</li>
+                <li>KY-16</li>
+                <li>KY-17</li>
+                <li>KY-18</li>
+                <li>KY-19</li>
+                <li>KY-20</li>
+                <li>KY-21</li>
+                <li>KY-22</li>
+                <li>KY-23</li>
+                <li>KY-24</li>
+                <li>KY-25</li>
+                <li>KY-26</li>
+                <li>KY-27</li>
+                <li>KY-28</li>
+                <li>KY-29</li>
+                <li>KY-30</li>
+                <li>KY-31</li>
+                <li>KY-32</li>
+                <li>KY-33</li>
+                <li>KY-34</li>
+                <li>KY-35</li>
+                <li>KY-36</li>
+                <li>KY-37</li>
+            </ol>
+        </div>
+        <div class="main-code">
+            <h1>KY-sensors</h1>
+            <section>
+            <h2>Welcome!</h2>
+            <p>Welcome to the KY-SENSORS!. A family of sensors that are widely used on different type of microcontrollers.
+            Here it be showed some arduino code that are simple examples of use. But also it will be added some examples at different languages to their respective Microcontrollers. As will be added some simple examples of code that implies just reading the values of the sensors or writing these values at the terminal,however will be useful to know some features of the sensors to take in mind when you are going to create a real project. 
+            So, as it'll be presented and explained some of the main features, schematics, besides it'll be presented a link to the official Datasheet or a link that you can download the PDF.
+            Otherwise, you might see some ideas that you could use some of these sensors on each page, so you could take some inspiration to make one of them or improve the idea with you perspective.
+            </p>
+            <h2>Guide to the website</h2>
+            <p>If you have some difficulties to understand the usage of the website you can read this part of the main page.
+                The index will be always at the left-side, so you can choose the sensor's number that you are looking for and have it always at your hand.
+                At the end of this page, you will find a footer that will be the link of my github, and you can also check some interesting projects that are in a sort of way, related with this one.
+                This is the first step of the project, I'd be charmed to add more sensors and explaining more interesting topics of microcontrolles taht can be useuful for everyone
+            </p>
+            <h2>Sensors</h2>
+            <p>You can look here a brief description for each sensors, as also will be the images of each one</p>
+        </section>
+        </div>
+        
+        
+    </main>
+</template>
