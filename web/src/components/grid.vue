06ceef9fa215eb84f9326b71b252086e3e94741c
Adrian <kerpador@gmail.com>
2024-08-10 16:52:32 -0600

    hola

--- web/src/components/grid.vue
+++ web/src/components/grid.vue
@@ -4,11 +4,14 @@ import{items,link} from '../library/items.ts';
 const  getImageUrl = (item: string) => {
     return new URL(`${link}${item}.webp`, import.meta.url).href;
   };
+  const  getImageUrl2 = (item: string) => {
+    return new URL(`./${item}.webp`, import.meta.url).href;
+  };
 </script>
 <template>
     <section class="catalogue">
         <div  v-for="item in items" :key="item" :class="item">
-            <img :src="getImageUrl(item)" alt="">
+            <img :srcset="`${getImageUrl(item)}, ${getImageUrl2(item)}`" alt="">
             <span>
                 {{item}}
             </span>
