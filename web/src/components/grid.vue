cb38d00a59493b7bef4ef0af8fd8baf9a3242a03
Adrian <kerpador@gmail.com>
2024-08-10 20:23:55 -0600

    add

--- web/src/components/grid.vue
+++ web/src/components/grid.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts" >
 import './grid.scss';
 import{items,link} from '../library/items.ts';
-const  getImageUrl = (item: string) => {
-    return new URL(`${link}${item}.webp`, import.meta.url).href;
-  };
+
+  const correctUrl=(item:string)=>{
+    const isDeploy = process.env.NODE_ENV === 'production'; // Detecta si estás en producción
+    if (isDeploy) {
+      // URL para entorno de despliegue
+      return new URL(`./${item}.webp`, import.meta.url).href;
+    } else {
+      // URL para entorno local
+       // Ajusta según tu configuración local
+       if (!item) {
+             return undefined; // Aquí podrías estar devolviendo undefined si item es falsy
+        }
+      return new URL(`${link}${item}.webp`, import.meta.url).href;
+    }
+  }
 </script>
 <template>
     <section class="catalogue">
         <div  v-for="item in items" :key="item" :class="item">
-            <img :src="getImageUrl(item)" alt="">
+            <img :src="`${correctUrl(item)}`" alt="">
             <span>
                 {{item}}
             </span>
